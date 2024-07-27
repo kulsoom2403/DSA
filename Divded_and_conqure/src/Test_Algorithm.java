0fa67da551e0d8084aa90e21edd728046801ec2c
kulsoom2403 <ukulsoom515@gmail.com>
2024-07-27 10:38:36 +0530

    Find Max and Min

---
+++ Divded_and_conqure/src/Test_Algorithm.java
@@ -0,0 +1,13 @@
+public class Test_Algorithm {
+
+    public static void main(String[] args) {
+
+            int a[] = {2, 1, 3, 4, 5, 6, 7, 8};
+
+
+             Straight_Min_Max.MinMax result=Straight_Min_Max.findElement(a);
+
+            System.out.println(result.min);
+            System.out.println(result.max);
+    }
+}
