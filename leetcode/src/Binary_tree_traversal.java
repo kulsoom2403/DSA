ba3ae7326effe93290cbf204eceedb998e1156e5
kulsoom2403 <ukulsoom515@gmail.com>
2024-09-06 19:20:59 +0530

    find level in bfs

---
+++ leetcode/src/Binary_tree_traversal.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Binary_tree_traversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> levelValues = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode current = queue.poll();
+                levelValues.add(current.val);
+
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+
+            res.add(levelValues);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        // Create Binary_tree_traversal object and call levelOrder method
+        Binary_tree_traversal solution = new Binary_tree_traversal();
+        List<List<Integer>> levels = solution.levelOrder(root);
+
+        // Print the result
+        for (int i = 0; i < levels.size(); i++) {
+            System.out.println("Level " + i + ": " + levels.get(i));
+        }
+    }
+}
--- leetcode/src/lowestCommanAncestor.java
+++ leetcode/src/lowestCommanAncestor.java
@@ -1,59 +1,59 @@
-// TreeNode class definition
-class TreeNode {
-    int val;
-    Tnode left;
-    Tnode right;
-
-    TreeNode(int x) {
-        val = x;
-    }
-}
-
-// Solution class containing the lowestCommonAncestor method
-class lca {
-    public Tnode lowestCommonAncestor(Tnode root, Tnode p, Tnode q) {
-        if (root == null) {
-            return null;
-        } else if (root == p || root == q) {
-            return root;
-        }
-
-        Tnode left = lowestCommonAncestor(root.left, p, q);
-        Tnode right = lowestCommonAncestor(root.right, p, q);
-
-        if (left == null) {
-            return right;
-        } else if (right == null) {
-            return left;
-        } else {
-            return root;
-        }
-    }
-}
-
-// Main class to test the code
- public class lowestCommanAncestor {
-    public static void main(String[] args) {
-        // Construct the binary tree
-        Tnode root = new Tnode(3);
-        root.left = new Tnode(5);
-        root.right = new Tnode(1);
-        root.left.left = new Tnode(6);
-        root.left.right = new Tnode(2);
-        root.right.left = new Tnode(0);
-        root.right.right = new Tnode(8);
-        root.left.right.left = new Tnode(7);
-        root.left.right.right = new Tnode(4);
-
-        // Create instances of p and q
-        Tnode p = root.left; // Node with value 5
-        Tnode q = root.left.right.right; // Node with value 4
-
-        // Call the lowestCommonAncestor method
-        lca solution = new lca();
-        Tnode lca = solution.lowestCommonAncestor(root, p, q);
-
-        // Print the result
-        System.out.println("Lowest Common Ancestor of nodes " + p.val + " and " + q.val + " is: " + lca.val);
-    }
-}
+//// TreeNode class definition
+//class TreeNode {
+//    int val;
+//    Tnode left;
+//    Tnode right;
+//
+//    TreeNode(int x) {
+//        val = x;
+//    }
+//}
+//
+//// Solution class containing the lowestCommonAncestor method
+//class lca {
+//    public Tnode lowestCommonAncestor(Tnode root, Tnode p, Tnode q) {
+//        if (root == null) {
+//            return null;
+//        } else if (root == p || root == q) {
+//            return root;
+//        }
+//
+//        Tnode left = lowestCommonAncestor(root.left, p, q);
+//        Tnode right = lowestCommonAncestor(root.right, p, q);
+//
+//        if (left == null) {
+//            return right;
+//        } else if (right == null) {
+//            return left;
+//        } else {
+//            return root;
+//        }
+//    }
+//}
+//
+//// Main class to test the code
+// public class lowestCommanAncestor {
+//    public static void main(String[] args) {
+//        // Construct the binary tree
+//        Tnode root = new Tnode(3);
+//        root.left = new Tnode(5);
+//        root.right = new Tnode(1);
+//        root.left.left = new Tnode(6);
+//        root.left.right = new Tnode(2);
+//        root.right.left = new Tnode(0);
+//        root.right.right = new Tnode(8);
+//        root.left.right.left = new Tnode(7);
+//        root.left.right.right = new Tnode(4);
+//
+//        // Create instances of p and q
+//        Tnode p = root.left; // Node with value 5
+//        Tnode q = root.left.right.right; // Node with value 4
+//
+//        // Call the lowestCommonAncestor method
+//        lca solution = new lca();
+//        Tnode lca = solution.lowestCommonAncestor(root, p, q);
+//
+//        // Print the result
+//        System.out.println("Lowest Common Ancestor of nodes " + p.val + " and " + q.val + " is: " + lca.val);
+//    }
+//}
