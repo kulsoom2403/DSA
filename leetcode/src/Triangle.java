0d116cc9872695a4be53ab94602fa0b28866c66b
Umme Kulsoom <ukulsoom515@gmail.com>
2025-03-29 02:22:09 +0530

    Given the nums array check the it is triangle are not

---
+++ leetcode/src/Triangle.java
@@ -0,0 +1,33 @@
+public class Triangle {
+
+        public String triangleType(int[] nums) {
+            if(nums.length != 3){
+                return"Invalid Triangle";
+            }
+            int a = nums[0];
+            int b = nums[1];
+            int c = nums[2];
+
+            if (a + b <= c || a + c <= b || b + c <= a) {
+                return"none";
+            }
+            if(a==b && b== c) {
+                return"equilateral";
+            }
+            else if(a == b || b == c || c==a) {
+                return"isosceles";
+            }
+            else {
+                return"scalene";
+            }
+
+
+        }
+
+    public static void main(String[] args) {
+        int[] nums ={3,4,5};
+        Triangle obj = new Triangle();
+        String result = obj.triangleType(nums);
+        System.out.println(result);
+    }
+}
