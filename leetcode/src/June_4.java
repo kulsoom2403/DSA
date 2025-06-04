66d02b3962c5803ac722bb0cff4e56958f484b92
Umme Kulsoom <ukulsoom515@gmail.com>
2025-06-04 09:53:01 +0530

    Find the Lexicographically Largest String From the Box I

---
+++ leetcode/src/June_4.java
@@ -0,0 +1,31 @@
+public class June_4 {
+
+        public String answerString(String word, int numFriends) {
+            if(numFriends == 1){
+                return word;
+            }
+            int n = word.length();
+
+
+            String res ="";
+            for(int i = 0; i < n;i++){
+                String s = word.substring(i,Math.min(i+n-numFriends+1,n));
+                if(res.compareTo(s)  <= 0){
+                    res = s;
+                }
+            }
+            return res;
+
+
+
+        }
+
+    public static void main(String[] args) {
+        String word = "abcd";
+        int numFriends = 2;
+        June_4 obj = new June_4();
+        String result = obj.answerString(word,numFriends);
+        System.out.println(result);
+
+    }
+}
