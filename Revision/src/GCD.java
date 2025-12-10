494bcfda218a9a0244cfe50bcf1dc39b5b32bac5
Umme Kulsoom <ukulsoom515@gmail.com>
2025-12-10 17:28:43 +0530

    find the GCD ,gcd mean greatest common divisor

---
+++ Revision/src/GCD.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GCD {
+    // Write your code here
+
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int result = 0;
+            for(int i = 1;i <= Math.min(a,b); i++){
+                if(a%i == 0 && b %i == 0){
+                    result = i;
+                }
+
+
+            }
+            System.out.println(result);
+        }
+
+}
