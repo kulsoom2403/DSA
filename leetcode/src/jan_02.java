a5d88880ecb2aa54a1af765fd90cbbc45d5fcaa9
Umme Kulsoom <ukulsoom515@gmail.com>
2026-01-02 16:31:15 +0530

    find the repeated elements in given the array

---
+++ leetcode/src/jan_02.java
@@ -0,0 +1,19 @@
+public class jan_02 {
+
+        public int repeatedNTimes(int[] nums) {
+            for (int i = 0; i < nums.length - 2; i++) {
+                if (nums[i] == nums[i + 1] || nums[i] == nums[i + 2]) {
+                    return nums[i];
+                }
+            }
+            return nums[nums.length - 1];
+        }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,2,1,3,1};
+        jan_02 obj = new jan_02();
+        int result = obj.repeatedNTimes(nums);
+        System.out.println(result);
+    }
+
+}
